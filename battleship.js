c5d5d284ccb3a69ced1581e42d3fb4124d8ad03b
hendro2019 <johnhendrickson9@gmail.com>
2020-08-22 17:16:22 -0500

    #4 Add member variables. Note the adition of 3 ship objects, holding location and numberof hits.

--- battleship.js
+++ battleship.js
@@ -1,3 +1,15 @@
+var model = {
+    boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+	
+	ships: [
+		{ locations: [0, 0, 0], hits: ["", "", ""] },
+		{ locations: [0, 0, 0], hits: ["", "", ""] },
+		{ locations: [0, 0, 0], hits: ["", "", ""] }
+	],
+};
 var view = {
 	displayMessage: function(msg) {
 		var messageArea = document.getElementById("messageArea");
