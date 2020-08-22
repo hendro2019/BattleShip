790dfeacfcf9cd52967e9199d8c2dc125d404f8f
hendro2019 <johnhendrickson9@gmail.com>
2020-08-22 17:38:45 -0500

    #4 Add fire function to determin if the guess is a hit. This functions makes uses of the methods in view to display results.

--- battleship.js
+++ battleship.js
@@ -8,7 +8,34 @@ var model = {
 		{ locations: [0, 0, 0], hits: ["", "", ""] },
 		{ locations: [0, 0, 0], hits: ["", "", ""] },
 		{ locations: [0, 0, 0], hits: ["", "", ""] }
-	],
+    ],
+
+    fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+
+			// Check to see if the ship
+			// has already been hit, message the user, and return true.
+			if (ship.hits[index] === "hit") {
+				view.displayMessage("Oops, you already hit that location!");
+				return true;
+			} else if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
 };
 var view = {
 	displayMessage: function(msg) {
