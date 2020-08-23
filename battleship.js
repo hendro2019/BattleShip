85d498533811e8702d0d33f8e51a8472a4ee8a08
hendro2019 <johnhendrickson9@gmail.com>
2020-08-23 11:52:52 -0500

    #5 two functions added, handleFireButton and handleKeyPress. Called when the fire! button is clicked or pressed respectivly.

--- battleship.js
+++ battleship.js
@@ -103,6 +103,30 @@ function parseGuess(guess) {
 	return null;
 }
 
+// event handlers
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value.toUpperCase();
+
+	controller.processGuess(guess);
+
+	guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+
+	// in IE9 and earlier, the event object doesn't get passed
+	// to the event handler correctly, so we use window.event instead.
+	e = e || window.event;
+
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+}
+
 window.onload = init;
 
 function init() {
