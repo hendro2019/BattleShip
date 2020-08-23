d77b1d0a7319215ce2dd951f67102f0c5fe8b0e7
hendro2019 <johnhendrickson9@gmail.com>
2020-08-23 12:45:58 -0500

    #7 add collision(). This method checks a new array of locatioins against existing ship locations. If there is a collision returns true, else returns false.

--- battleship.js
+++ battleship.js
@@ -81,6 +81,18 @@ var model = {
 		return newShipLocations;
 	},
 
+	collision: function(locations) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			for (var j = 0; j < locations.length; j++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 };
 
 var view = {
